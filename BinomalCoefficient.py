0900f2cb468d7c4c454176e9530d72c53349e683
JeongGyu Kim (zzqyu) <wjdrb0626@gmail.com>
2018-04-13 00:13:47 +0900

    init, DynamicPrograming
    BinomalCoefficient
    Floyd

---
+++ BinomalCoefficient.py
@@ -0,0 +1,50 @@
+﻿import time
+
+class Coefficient:
+	def __init__(self):
+		Coefficient.C = []
+	def Recursion(n, r):
+		if n==r or r==0:
+			return 1
+		return Coefficient.Recursion(n-1, r-1) + Coefficient.Recursion(n-1, r)
+
+	def Memoization(n, r):
+		Coefficient.C = [[None]*(n+1) for i in range(n+1)]
+		return Coefficient.Recursion2(n, r)
+
+	def Recursion2(n, r):
+		if Coefficient.C[n][r] == None:
+			if n==r or r==0:
+				Coefficient.C[n][r] = 1
+			else:
+				Coefficient.C[n][r] = Coefficient.Recursion(n-1, r-1) + Coefficient.Recursion(n-1, r)
+		
+		return Coefficient.C[n][r]
+	
+	def DynamicPrograming(n, r):
+		Coefficient.C = [[None]*(n+1) for i in range(n+1)]
+		for _n in range(n+1):
+			for _r in range(_n+1):
+				if _n==_r or _r==0:
+					Coefficient.C[_n][_r] = 1
+				else:
+					Coefficient.C[_n][_r] = Coefficient.C[_n-1][_r-1] + Coefficient.C[_n-1][_r]
+		return Coefficient.C[n][r]
+##테스트코드
+if __name__ == "__main__" :
+	n = 25
+	r = 23
+	startTime = time.time()
+	print (Coefficient.Recursion(n, r))
+	print ("time ", time.time()-startTime)
+	startTime = time.time()
+	print (Coefficient.Memoization(n, r))
+	print ("time ", time.time()-startTime)
+	startTime = time.time()
+	print (Coefficient.DynamicPrograming(n, r))
+	print ("time ", time.time()-startTime)
+
+	
+	
+
+
---
+++ Floyd.py
@@ -0,0 +1,37 @@
+﻿import time
+
+class Floyd:
+	def __init__(self):
+		Floyd.W = [[],
+				[None, 0, 1, -1, 1, 5],
+				[None, 9, 0, 3, 2, -1],
+				[None, -1, -1, 0, 4, -1],
+				[None, -1, -1, 2, 0, 3],
+				[None, 3, -1, -1, -1, 0],
+				]
+		'''self.D = [[None]*(i+1) for n in range(j+1)]
+		self.i = i
+		self.j = j'''
+
+	
+
+	def Min(a, b):
+		if a==-1:
+			a=100
+		return a<=b and a or b
+
+	def Recursion(self, k, i, j):
+		if k==0 and Floyd.W[i][j] >= 0:
+			return Floyd.W[i][j]
+		return Floyd.Min(self.Recursion(k-1, i, j), self.Recursion(k-1, i, k) + self.Recursion(k-1, k, i))
+		
+	
+##테스트코드
+if __name__ == "__main__" :
+	f = Floyd()
+	print(f.Recursion(4, 1, 3))
+
+	
+	
+
+
